31a41fd33cd5d45ec07b37a937458122b17207e1
anay333 <anay_1991@bk.ru>
2021-10-03 23:32:45 +0500

    added files

---
+++ src/main/java/domain/Vkpost.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Vkpost {
+    private String id;
+    private String imageId;
+    private String name;
+    private String labelId;
+    private String date;
+    private String text;
+    private String buttonUrl;
+    private String mainTxtOnTheImage;
+    private String descriptionTxtOnTheImage;
+    private String textButton;
+    private String picOnTheButton;
+    private String likeClick;
+    private String commentClick;
+    private String shareClick;
+    private String watchedPic;
+    private String watchedCount;
+    private String favoriteStar;
+
+    //get+set
+}
